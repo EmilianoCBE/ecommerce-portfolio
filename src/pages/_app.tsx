6758330f024b5b13102e6c35d0cecc165abd120a
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-07 13:12:32 -0600

    Spartan font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,20 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { extendTheme } from '@chakra-ui/react'
+
+import '@fontsource/spartan/400.css'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Spartna', sans-serif`,
+    body: `'Spartan', sans-serif`,
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>  
   )
