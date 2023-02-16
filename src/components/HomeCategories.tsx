ddc3a159e0c024c0b403ca10aca6835474821211
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-16 13:07:11 -0600

    Add responsive to categories

--- src/components/HomeCategories.tsx
+++ src/components/HomeCategories.tsx
@@ -13,27 +13,33 @@ type Props = {
 
 export function HomeCategories({categories}: Props) {
     return (
-    <Grid templateColumns="540px 255px 255px" gap="30px" templateRows="200px 260px">
-        {categories.map((cat, key) => {
+    <Grid templateColumns={{
+        base: '1fr 1fr',
+        sm: '540px 255px 255px'
+    }} templateRows={{
+        base: '130px 154px 130px',
+        sm: '200px 260px'
+    }} gap={{
+        base: '0.5rem',
+        sm: '30px'
+    }} templateAreas={{
+        base: `
+            'cat1 cat1'
+            'cat2 cat3'
+            'cat4 cat4'
+        `,
+        sm: `
+            'cat1 cat2 cat3'
+            'cat1 cat4 cat4'
+        `,
+    }}>
+        {categories.map((cat, index) => {
                 const slug = slugify(cat);
                 const imageUrl = `/pic-categories-${slug}.jpg`;
-                let props: GridItemProps = {
-                    position: 'relative',
-                    w: '100%',
-                    h: '100%'
-                };
-
-                if (key === 0) {
-                    props.rowSpan = 2;
-                }
-
-                if (key === categories.length - 1) {
-                    props.colSpan = 2;
-                }
 
                 return (
-                    <GridItem {...props} key={key}>
-                        <Image src={imageUrl} fill={true} alt={cat} />
+                    <GridItem fontSize={{base: '0.85rem', sm: '1rem'}} position='relative' w='100%' h='100%' gridArea={`cat${index + 1}`} key={index}>
+                        <Image src={imageUrl} fill={true} alt={cat} style={{objectFit: 'cover'}}/>
                         <CenteredLabel>{cat}</CenteredLabel>
                     </GridItem>
                 );
