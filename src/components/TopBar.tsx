2da4bf7b6cea225f03758bc615e1447cc8b6e6da
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-16 12:22:38 -0600

    Mobile topbar and header

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -8,22 +8,36 @@ export function TopBar() {
     <Box bg='black' w='100%' p={2}>
       <Container as={Flex} color='white' justifyContent='space-between' fontSize="xs" size="lg">
           <Flex gap="1.5rem">
-              <Flex as={Link} href='#' alignItems="center" gap="0.5rem"><Image src="/icon-small-phone.svg" alt="" width={24} height="24"/>+38 (050) 12 34 567</Flex>
+              <Flex as={Link} href='#' alignItems="center" gap="0.5rem">
+                <Image src="/icon-small-phone.svg" alt="" width={24} height="24"/>+38 (050) 12 34 567
+              </Flex>
               
-              <Flex as={Link} href='#' alignItems="center" gap="0.5rem"><Image src="/icon-small-location.svg" alt="" width={24} height="24"/>Ukraine, Kyiv, Khreschatyk 1</Flex>
+              <Flex as={Link} href='#' alignItems="center" gap="0.5rem" display={{base:'none' , sm:'flex'}}>
+                <Image src="/icon-small-location.svg" alt="" width={24} height="24"/>Ukraine, Kyiv, Khreschatyk 1
+              </Flex>
               
-              <Flex as={Link} href='#' alignItems="center" gap="0.5rem"><Image src="/icon-small-clock.svg" alt="" width={24} height="24"/>All week 24/7</Flex>
+              <Flex as={Link} href='#' alignItems="center" gap="0.5rem" display={{base:'none' , sm:'flex'}}>
+                <Image src="/icon-small-clock.svg" alt="" width={24} height="24"/>All week 24/7
+              </Flex>
           </Flex>
               
 
           <Flex gap="1rem">
-              <Link href="#"><Image src="/icon-facebook.svg" alt="" width={24} height="24"/></Link>
+              <Link href="#">
+                <Image src="/icon-facebook.svg" alt="" width={24} height="24"/>
+              </Link>
               
-              <Link href="#"><Image src="/icon-twiter.svg" alt="" width={24} height="24"/></Link>
+              <Link href="#">
+                <Image src="/icon-twiter.svg" alt="" width={24} height="24"/>
+              </Link>
               
-              <Link href="#"><Image src="/icon-instagram.svg" alt="" width={24} height="24"/></Link>
+              <Link href="#">
+                <Image src="/icon-instagram.svg" alt="" width={24} height="24"/>
+              </Link>
               
-              <Link href="#"><Image src="/icon-pinterest.svg" alt="" width={24} height="24"/></Link>
+              <Link href="#">
+                <Image src="/icon-pinterest.svg" alt="" width={24} height="24"/>
+              </Link>
           </Flex>  
         </Container>          
     </Box>
