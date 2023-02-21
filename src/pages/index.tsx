ccd15a39d145cfac563608c34a23b272b92d2b21
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-20 18:21:59 -0600

    Scroll por products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,13 +51,25 @@ export default function Home({products, categories}: Props) {
           <HomeCategories categories={categories}/>
 
           <AdvantageSection/>
-          
-          {<SimpleGrid minChildWidth='255px' spacing='1.85rem'>
+
+        </Container>  
+
+          {<SimpleGrid minChildWidth={{
+            lg:'255px'
+          }} 
+            overflowX='scroll'
+            gridTemplateColumns='repeat(auto-fit, 255px)' 
+            gridAutoColumns='255px'
+            gridAutoFlow='column'
+            spacing='1.85rem'
+            scrollSnapType='x mandatory'
+            >
             {products.map(product => {
-              return <ProductCard {...product} key={product.id}/>
+              return <Box key={product.id} scrollSnapAlign='center'>
+                      <ProductCard {...product}/>
+                     </Box>
             })}
           </SimpleGrid>} 
-        </Container>  
         
       </main>
     </>
