7506cff94077d59495daad16d185569e8899b3ec
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-22 18:15:29 -0600

    Add responsive to women category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { AdvantageSection } from '@/components/AdvantageSection'
 
 import { Categories } from '@/models/Categories'
 
-import { Box, Container, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Container, Flex, Grid, SimpleGrid, Text } from '@chakra-ui/react'
 import { ProductCard } from '@/components/ProductCard'
 
 export type Product = {
@@ -54,22 +54,43 @@ export default function Home({products, categories}: Props) {
 
         </Container>  
 
-          {<SimpleGrid minChildWidth={{
-            lg:'255px'
-          }} 
-            overflowX='scroll'
-            gridTemplateColumns='repeat(auto-fit, 255px)' 
-            gridAutoColumns='255px'
-            gridAutoFlow='column'
-            spacing='1.85rem'
-            scrollSnapType='x mandatory'
+        <Container maxW={{
+          base: '100%',
+          md: '1110px'
+        }}>
+          <Grid 
+              overflowX='scroll'
+              gridTemplateColumns={{
+                base: 'repeat(auto-fit, 255px)',
+                md: 'repeat(auto-fill, minmax(255px, 1fr))' 
+              }}
+              gridAutoColumns='255px'
+              gridAutoRows='1fr'
+              gridAutoFlow={{
+                base: 'column',
+                md: 'row'
+              }}
+              gap='1.85rem'
+              scrollSnapType='x mandatory'
+              alignItems='stretch'
             >
-            {products.map(product => {
-              return <Box key={product.id} scrollSnapAlign='center'>
+            {products.map((product,i) => {
+              return <Box 
+                        marginLeft={{
+                          base: i === 0? '1rem' : '0',
+                          md: '0'
+                        }}
+                        key={product.id} 
+                        scrollSnapAlign='center' 
+                        border='solid 1px' 
+                        borderColor='gray.200' 
+                        padding='1rem'
+                      >
                       <ProductCard {...product}/>
                      </Box>
             })}
-          </SimpleGrid>} 
+          </Grid> 
+        </Container>
         
       </main>
     </>
