d0b9d07a64a802c04fa02e3eb643791b029f77c7
EmilianoCBE <e.carrizales1305@gmail.com>
2023-03-04 22:10:16 -0600

    Style changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, color } from '@chakra-ui/react'
 
 import { extendTheme } from '@chakra-ui/react'
 
@@ -9,12 +9,38 @@ import { defineStyleConfig} from '@chakra-ui/react'
 
 export const buttonTheme = defineStyleConfig({
   baseStyle: {
-    fontWeight: 'bold',
+    fontWeight: 'normal',
     textTransform: 'uppercase',
     borderRadius: 'none',
+    paddingX: '1.5rem',
+    _hover: {
+      backgroundColor: 'brand.800'
+    }
+  },
+  sizes:{
+    sm:{
+      fontSize: '0.75rem',
+    },
+    md:{
+      fontSize: '0.8125rem',
+    },
+    lg:{
+      fontSize: '0.8125rem'
+    },
+    xl:{
+      fontSize: '1rem',
+      height: '3.5556rem',
+      paddingX: '3rem'
+    }
   },
   defaultProps: {
-    colorScheme: 'brand'
+    variant: 'primary'
+  },
+  variants: {
+    primary: {
+      backgroundColor: 'brand.900',
+      color: 'white'
+    }
   }
 })
 
